ae3ebee39334286fd2ab3e9a28dcbc39a9328dd3
Isaac DT <ideltoro136@gmail.com>
2018-11-10 23:45:07 -0700

    Query but no ouput from array

--- src/components/dailydoodle.js
+++ src/components/dailydoodle.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import styles from './dailydoodle.module.css'
 
-const DailyDoodle = () => (
+const DailyDoodle = ({ word }) => (
   <div className={styles.pageTitle}>
     <div className={styles.doodleOfTheDayWrapper}>
       <div className={styles.doodleOfTheDayCell}>
-        <h1>Pocket</h1>
+        <h1>{word}</h1>
       </div>
     </div>
   </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,38 @@
 import React from 'react'
-
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import DailyDoodle from '../components/dailydoodle'
 
-const IndexPage = () => (
-  <Layout>
-    <div
-      style={{
-        maxWidth: '300px',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-      }}
-    >
-      <DailyDoodle />
-    </div>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  console.log(data.allDataYaml.edges)
+  return (
+    <Layout>
+      <div
+        style={{
+          maxWidth: '300px',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+        }}
+      >
+        {/*<p>{data.allDataYaml.edges.node.words.map(,1)}</p>*/}
+        <DailyDoodle word={'Cheese'} />
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query WordsQuery {
+    allDataYaml {
+      edges {
+        node {
+          words {
+            word
+          }
+        }
+      }
+    }
+  }
+`
