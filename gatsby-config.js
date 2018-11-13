4a2b94fdd399df684d6566d09c7f22f96bb32b44
Isaac DT <ideltoro136@gmail.com>
2018-11-13 12:03:35 -0700

    Moved the Manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,18 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Daily Doodle App',
+        short_name: 'Daily Doodle',
+        start_url: '/',
+        background_color: '#ff3333',
+        theme_color: '#121212',
+        display: 'minimal-ui',
+        icon: 'src/images/ddicon.png', //This path is relative to the root of the site.
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
@@ -21,18 +33,7 @@ module.exports = {
         path: `${__dirname}/src/data/`,
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'Daily Doodle App',
-        short_name: 'Daily Doodle',
-        start_url: '/',
-        background_color: '#ff3333',
-        theme_color: '#121212',
-        display: 'minimal-ui',
-        icon: 'src/images/ddicon.png', //This path is relative to the root of the site.
-      },
-    },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     'gatsby-plugin-offline',
